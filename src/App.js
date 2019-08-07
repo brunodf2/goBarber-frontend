00994e9b26777605fde3b4c5f27c8e13ff430d9a
brunodf2 <bruno.araujodf2@gmail.com>
2019-08-07 00:43:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+
+import './config/RectotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
+
+import { store, persistor } from './store';
+
+import GlobalStyles from './styles/global';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyles />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default App;
